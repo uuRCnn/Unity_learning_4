8f2a1cb5731386c95c27477db56f210bb980bc30
Ugurcan <ugurcan380@gmail.com>
2023-02-28 23:18:03 +0300

    ss

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,18 +6,33 @@ using DG.Tweening;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject karePrefab;
-
     [SerializeField] private GameObject karelerPaneli;
-
     [SerializeField] private Transform SoruPaneli;
-    
+    [SerializeField] private TextMeshProUGUI soruTexti;
+    [SerializeField] private Sprite[] kareSpirtes;
+    private int bölünensayı, bölensayı, kacıncıSoru, buttondegeri, dogru_sonuc, kalan_hak;
+    private string sorununZorlukDerecesi;
+    private bool butona_basıldımı = false;
     private GameObject[] karelerDizisi = new GameObject[25];
+    private GameObject gecerliKare;
+    private kalanCanlar Kalan_Canlar;
+    private PuanManager _puanManager;
 
+    List<int> bolumDegerlerilistesi = new List<int>();
+
+    private void Awake()
+    {
+        kalan_hak = 3;
+        Kalan_Canlar = Object.FindObjectOfType<kalanCanlar>();
+        _puanManager = Object.FindObjectOfType<PuanManager>();
+        Kalan_Canlar.Kalan_Hakları_Kontrol_et(kalan_hak);
+    }
 
     private void Start()
     {
@@ -30,14 +45,45 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < karelerDizisi.Length; i++)
         {
             karelerDizisi[i] = Instantiate(karePrefab);
+            karelerDizisi[i].transform.GetChild(1).GetComponent<Image>().sprite = kareSpirtes[Random.Range(0,kareSpirtes.Length)];
+            karelerDizisi[i].transform.GetChild(1).GetComponent<Image>().enabled = false;
+            karelerDizisi[i].transform.GetComponent<Button>().onClick.AddListener(() => buttonaBasıldı());
             karelerDizisi[i].transform.parent = karelerPaneli.transform;
         }
+
         StartCoroutine(DoFadeRoutine());
         BolumDegerliniTexteYazdır();
         Invoke("SoruPaneliniAç", 2f);
-
     }
 
+    void buttonaBasıldı()
+    {
+        if (butona_basıldımı)
+        {
+            buttondegeri = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform
+                .GetChild(0).GetComponent<TextMeshProUGUI>().text);
+            gecerliKare = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+            Sonucu_Konrtolet();
+        }
+    }
+
+    void Sonucu_Konrtolet()
+    {
+        if (buttondegeri == dogru_sonuc)
+        {
+            gecerliKare.transform.GetChild(1).GetComponent<Image>().enabled = true;
+            gecerliKare.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = " ";
+            gecerliKare.transform.GetComponent<Button>().interactable = false;
+            _puanManager.PuaniArtir(sorununZorlukDerecesi);
+            bolumDegerlerilistesi.RemoveAt(kacıncıSoru);
+            SoruPaneliniAç();
+        }
+        else
+        {
+            kalan_hak--;
+            Kalan_Canlar.Kalan_Hakları_Kontrol_et(kalan_hak);
+        }
+    }
     IEnumerator DoFadeRoutine()
     {
         foreach (var kare in karelerDizisi)
@@ -51,13 +97,39 @@ public class GameManager : MonoBehaviour
     {
         foreach (var kare in karelerDizisi)
         {
-            int RasgeleDeger = Random.Range(1, 13);
+            int RasgeleDeger = Random.Range(2, 13);
+            bolumDegerlerilistesi.Add(RasgeleDeger);
             kare.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = RasgeleDeger.ToString();
         }
     }
 
     void SoruPaneliniAç()
     {
+        SoruyuSor();
+        butona_basıldımı = true;
+        Debug.Log(kacıncıSoru);
         SoruPaneli.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+    }
+
+    void SoruyuSor()
+    {
+        bölensayı = Random.Range(2, 11);
+        kacıncıSoru = Random.Range(0, bolumDegerlerilistesi.Count);
+        dogru_sonuc = bolumDegerlerilistesi[kacıncıSoru];
+        bölünensayı = bölensayı * dogru_sonuc;
+        soruTexti.text = bölünensayı.ToString() + " : " + bölensayı.ToString();
+        
+        if (bölünensayı >=20)
+        {
+            sorununZorlukDerecesi = "kolay";
+        }
+        else if (bölünensayı >=40 && bölensayı <= 60)
+        {
+            sorununZorlukDerecesi = "orta";
+        }
+        else
+        {
+            sorununZorlukDerecesi = "zor";
+        }
     }
 }
